f1eb4a462f3db42d400d2642ecdd1b887389dcdd
Tomek <tkrutowski@gmail.com>
2025-12-28 11:42:38 +0100

    migrate to PrimeUI themes and replace custom Tailwind setup

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,47 @@
 import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import './style.css';
 import './assets/tailwind.css';
 import ConfirmationService from 'primevue/confirmationservice';
 import DialogService from 'primevue/dialogservice';
 import ToastService from 'primevue/toastservice';
+import Aura from "@primeuix/themes/aura";
+import { definePreset } from '@primeuix/themes';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+const MyPreset = definePreset(
+  Aura, {
+    semantic: {
+      primary: {
+        50: "{yellow.50}",
+        100: "{yellow.100}",
+        200: "{yellow.200}",
+        300: "{yellow.300}",
+        400: "{yellow.400}",
+        500: "{yellow.500}",
+        600: "{yellow.600}",
+        700: "{yellow.700}",
+        800: "{yellow.800}",
+        900: "{yellow.900}",
+        950: "{yellow.950}",
+      },
+    },
+  }
+);
+
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, {
-  theme: 'none',
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: '.p-dark',
+    },
+  },
   locale: {
     accept: 'Tak',
     addRule: 'Dodaj regułę',
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     vueDevTools(),
     Components({
       resolvers: [PrimeVueResolver()],
